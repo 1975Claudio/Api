5d0b3147d4f2f60bdd85b59010b1f7f241013679
CLAUDIO_ALMEIDA <157855960+BackEndInnovation@users.noreply.github.com>
2024-07-12 21:56:07 -0300

    Primeiro Commit

--- api/src/main/java/mad/voll/api/controller/MedicoController.java
+++ api/src/main/java/mad/voll/api/controller/MedicoController.java
@@ -1,14 +1,15 @@
 package mad.voll.api.controller;
 
+import jakarta.validation.Valid;
 import mad.voll.api.medico.DadosCadastroMedico;
+import mad.voll.api.medico.DadosListagemMedico;
 import mad.voll.api.medico.Medico;
 import mad.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping ("medicos")
@@ -18,7 +19,11 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar (@RequestBody DadosCadastroMedico dados) {
+    public void cadastrar (@RequestBody @Valid DadosCadastroMedico dados) {
        repository.save(new Medico(dados));
+    }
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+      return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
